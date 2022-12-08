f9fe75ac4b4ec2f9ad94119fab2bfc680c83b924
johnemechete <johnemechete@gmail.com>
2022-12-08 05:16:45 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
+/**
+* main - Run time starts here
+* Description: print a strings
+* Return: return 0 if successful
+*/
 int main()
 {
-char str1[] = "Programming is like building a multilingual puzzle";
+char str1[] = "\"Programming is like building a multilingual puzzle";
 puts(str1);
 return (0);
 }
