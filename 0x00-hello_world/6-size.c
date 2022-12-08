b7b8d0266c15292dbbd0f85bc837e7d50fdf694c
johnemechete <johnemechete@gmail.com>
2022-12-08 04:59:43 -0800

    6-siz.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+* main - run time start from here
+*
+* Description: This code is to print out the vairious datatype size
+*
+* Return: return 0 if successful
+**/
+int main()
+{
+printf("Size of a char: %lu byte(s)\n", sizeof(char));
+printf("Size of an int: %lu byte(s)\n", sizeof(int));
+printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
+printf("Size of a float: %lu byte(s)\n", sizeof(float));
+return (0);
+}
