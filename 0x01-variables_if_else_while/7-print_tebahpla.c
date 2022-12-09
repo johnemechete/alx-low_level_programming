24f56df244e4d6aecdcae606df64a626d3449e16
johnemechete <johnemechete@gmail.com>
2022-12-09 07:28:16 -0800

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+* main - Runtime start here
+* Description: This code is to tell us if the number is negative or positive
+* Return: return 0 if run successful
+*/
+int main(void)
+{
+	int c;
+
+	for (c = 122 ; c > 96; c--)
+		putchar(c);
+	putchar('\n');
+	return (0);
+}
