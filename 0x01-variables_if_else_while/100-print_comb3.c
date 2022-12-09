5e4b5a9f05f96608f1fd66a2a9ec182c2be884dc
johnemechete <johnemechete@gmail.com>
2022-12-09 09:32:43 -0800

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+* main - Runtime start here
+* Description: This code is to tell us if the number is negative or positive
+* Return: return 0 if run successful
+*/
+int main(void)
+{
+	int c =48, d = 48, e = 0;
+
+	for (c; c < 57; c++)
+	{
+		for (e = e + d; e < 58; e++)
+		{
+
+			if (c == e)
+				continue;
+			if (c != 56 || e != 57)
+			{
+				putchar(44);
+				putchar(32);
+			}
+		}
+		d++;
+		e = 0;
+	}
+	putchar('\n');
+	return (0);
+}
