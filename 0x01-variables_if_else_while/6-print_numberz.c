44aaa3e9ab906d3441828a09b78a127a1e2bf0bb
johnemechete <johnemechete@gmail.com>
2022-12-09 07:20:13 -0800

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+* main - Runtime start here
+* Description: This code is to tell us if the number is negative or positive
+* Return: return 0 if run successful
+*/
+int main(void)
+{
+	int c = 48;
+
+	for (c = 0; c < 57; c++)
+		putchar(c);
+	putchar('\n');
+	return (0);
+}
