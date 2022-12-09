618c492d12c3057d1f36214fcc4905d17a4fe2eb
johnemechete <johnemechete@gmail.com>
2022-12-09 07:13:27 -0800

    5-print_numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+* main - Runtime start here
+* Description: This code is to tell us if the number is negative or positive
+* Return: return 0 if run successful
+*/
+int main(void)
+{
+	int c = 0;
+
+	for (c = 0; c < 11; c++)
+		printf("%d", c);
+	putchar('\n');
+	return (0);
+}
