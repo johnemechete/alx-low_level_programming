36bea266b1336e193aeccc629236c1ddc7484098
johnemechete <johnemechete@gmail.com>
2022-12-09 05:02:58 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+* main - Runtime start here
+* Description: This code is to tell us if the number is negative or positive
+* Return: return 0 if run successful
+*/
+int main(void)
+{
+	int c = 97;
+
+	while (c < 122)
+	{
+		putchar(c);
+		++c;
+	}
+	return (0);
+}
