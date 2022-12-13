74760151a768f166cd54c05395f6d37117771094
johnemechete <johnemechete@gmail.com>
2022-12-13 06:39:48 -0800

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* print_sign- To print the alphabet
+* @n: takes an int value
+* Return: return 0 if it is an Alphabet  case else 1
+*/
+
+int print_sign(int n)
+{
+	if (n > 0)
+		return (1);
+	else
+		return (0);
+
+}
