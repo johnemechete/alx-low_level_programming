70fad87160c512b0b3b9876ea1566c6369cb7c4a
johnemechete <johnemechete@gmail.com>
2022-12-13 14:29:55 -0800

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* add - To  adds two integers and returns the result
+* followed by a new line
+* @a: takes an int value
+* @b: takes an int value
+* Return: return 0 if it is an Alphabet  case else 1
+*/
+
+int add(int a, int b)
+{
+	return (a + b);
+}
