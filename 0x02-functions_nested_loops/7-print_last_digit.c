a3c311f656fc3da1c2ae05c24472666c2c23d805
johnemechete <johnemechete@gmail.com>
2022-12-13 07:36:02 -0800

    7-main.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
-* print_last_digit - To prints the last digit of a number.
+* print_last_digit- To prints the last digit of a number.
 * @n: takes an int value
 * Return: return 0 if it is an Alphabet  case else 1
 */
@@ -8,5 +8,9 @@
 int print_last_digit(int n)
 {
 	n = n % 10;
+	if (n > 0)
+		n = n;
+	else
+		n = (-1) * n;
 	return (n);
 }
