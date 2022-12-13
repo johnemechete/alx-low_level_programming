055cbffbb542dd63273c6c17f83bea592ebb11bc
johnemechete <johnemechete@gmail.com>
2022-12-13 08:31:47 -0800

    8-main.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,8 +1,7 @@
 #include "main.h"
 /**
-* jack_bauer - To  prints every minute of the day of Jack Bauer, 
+*  jack_bauer- To  prints every minute of the day of Jack Bauer, 
 * starting from 00:00 to 23:59
-* @n: takes an int value
 * Return: return 0 if it is an Alphabet  case else 1
 */
 
@@ -14,10 +13,10 @@ void jack_bauer(void)
 	for (a = 0; a <= 2; a++)
 	{
 		if (a == 2)
-			f = 4;
+			f = 3;
 		for (b = 0; b <= f; b++)
 		{
-			for (c = 0; c <= 6; c++)
+			for (c = 0; c <= 5; c++)
 			{
 				for (d = 0; d <= 9; d++)
 				{
