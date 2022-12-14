2e01b2490a6681616c39703334b416114fe11896
johnemechete <johnemechete@gmail.com>
2022-12-14 04:23:28 -0800

    fib series

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,23 +1,21 @@
-nclude <stdio.h>
+#include <stdio.h>
 /**
-   * main -prints finonacci numbers
-    * Return: Always 0
-     */
-
+ * main -prints finonacci numbers
+ * Return: Always 0
+ */
 int main(void)
 {
-		signed long int old = 1, new = 2, sum = 0, add = 0;
-
-			add = new;
-				sum = old + new;
-					while (sum < 4000000)
-							{
-										if (sum % 2 == 0)
-														add = add + sum;
-												old = new;
-														new = sum;
-																sum = old + new;
-																	}
-						printf("%ld\n", add);
-								return (0);
+	signed long int old = 1, new = 2, sum = 0, add = 0;
+	add = new;
+	sum = old + new;
+	while (sum < 4000000)
+	{
+		if (sum % 2 == 0)
+			add = add + sum;
+		old = new;
+		new = sum;
+		sum = old + new;
+	}
+	printf("%ld\n", add);
+	return (0);
 }
