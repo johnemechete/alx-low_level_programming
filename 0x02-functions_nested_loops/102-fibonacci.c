b6faa0f6ac257d3be0f9acb1dff3567b192a9e3d
johnemechete <johnemechete@gmail.com>
2022-12-14 04:16:57 -0800

    fibonacci

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,24 +1,23 @@
-nclude <stdio.h>
+#include <stdio.h>
 /**
-   * main -prints finonacci numbers
-    * Return: Always 0
-     */
-
+ * main -prints finonacci numbers
+ * Return: Always 0
+ */
 int main(void)
 {
-		int num;
-			signed long int old = 1, new = 2, sum = 0;
+	int num;
+	signed long int old = 1, new = 2, sum = 0;
 
-				printf("%ld, ", old);
-					printf("%ld, ", new);
-						sum = old + new;
-							for (num = 3; num < 50; num++)
-									{
-												printf("%ld, ", sum);
-														old = new;
-																new = sum;
-																		sum = old + new;
-																			}
-								printf("%ld\n", sum);
-										return (0);
+	printf("%ld, ", old);
+	printf("%ld, ", new);
+	sum = old + new;
+	for (num = 3; num < 50; num++)
+	{
+		printf("%ld, ", sum);
+		old = new;
+		new = sum;
+		sum = old + new;
+	}
+	printf("%ld\n", sum);
+	return (0);
 }
