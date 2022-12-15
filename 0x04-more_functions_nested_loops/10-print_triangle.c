1c4cf51bfb7095fc471217b2f374094b8534e916
johnemechete <johnemechete@gmail.com>
2022-12-15 07:21:29 -0800

    ./10-triangles

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+* print_triangle - To print the alphabet
+* @size: takes an int value
+* Return: return 0 if it is lower case else 1
+*/
+
+void print_triangle(int size)
+{
+	int row, col, rev;
+
+	for (row = 0; row < size; row++)
+	{
+		rev = size - (1 + row);
+		for (col = 0; col < size; col++)
+		{
+			if (rev != 0)
+			{
+				_putchar(' ');
+				rev--;
+			}
+			else
+				_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
