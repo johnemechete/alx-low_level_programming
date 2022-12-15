aa0594b9dc174d60e2190bad00254f29ac8a40e8
johnemechete <johnemechete@gmail.com>
2022-12-15 10:53:49 -0800

    task 1

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -2,15 +2,13 @@
 /**
 * _isdigit - To print the alphabet
 * @c: takes an int value
-* @zero: the first digit
-* @nine: the last digit
 * Return: return 0 if it is lower case else 1
 */
 
 int _isdigit(int c)
 {
-	int zero = '0';
-	int nine = '9';
+	int zero = 48;
+	int nine = 57;
 
 	if (c >= zero && c <= nine)
 		return (1);
