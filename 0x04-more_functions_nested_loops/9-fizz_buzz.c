c856bbc5bc8b519d96bed4ffd99ab75402f4e4ac
johnemechete <johnemechete@gmail.com>
2022-12-15 05:50:21 -0800

    Task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,11 +14,11 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if ((i % 3) == 0)
+		if ((i % 5) == 0)
 		{
 			printf("Buzz ");
 		}
-		else if ((i % 5) == 0)
+		else if ((i % 3) == 0)
 		{
 			printf("Fuzz ");
 		}
