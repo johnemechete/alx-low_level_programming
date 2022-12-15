e0ac135f33ecf2d64103050e35f8e132da44c871
johnemechete <johnemechete@gmail.com>
2022-12-15 11:33:40 -0800

    100_task

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+* main - To print the alphabet
+* Description: takes an int value
+* Return: return 0 if it is lower case else 1
+*/
+
+int main(void)
+{
+	unsigned long int n, i;
+
+	n = 612852475143;
+	i = 2;
+
+	while (n != i)
+	{
+		if (n % 2 == 0)
+		{
+			n = n / 2;
+		}
+		else
+		{
+			i++;
+		}
+	}
+
+	printf("%lu \n", n);
+	return (0);
+}
