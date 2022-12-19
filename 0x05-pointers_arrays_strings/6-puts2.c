1eeeed8426699e8566cf2752d723ccb264a7405c
johnemechete <johnemechete@gmail.com>
2022-12-19 06:24:29 -0800

    6-main.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,13 +9,20 @@
 void puts2(char *str)
 {
 	int i = 0;
+	int len;
 
 	while (str[i] != '\0')
+	{
+		i++;
+	}
+	len = i - 1;
+
+
+	for (i = 0; i <= len; i++)
 	{
 		if (i != 0)
 			i++;
 		_putchar(str[i]);
-		i++;
 	}
 	_putchar('\n');
 }
