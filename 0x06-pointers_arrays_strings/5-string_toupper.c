d80ba893670c82f3eb722cc99ad654f1edf975d4
johnemechete <johnemechete@gmail.com>
2022-12-21 04:18:20 -0800

    5-main.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -2,13 +2,26 @@
 
 /**
 * string_toupper - concatenates two strings.
-* @de: the main string
+* @s: the main string
 * Return: returns a string
 */
 
 char *string_toupper(char *s)
 {
 
-	return (dest);
+	int i;
+
+	for (i = 0; s[i] != 0; i++)
+	{
+		if (isalpha(s[i]))
+		{
+			if (islower(s[i]))
+				s[i] = s[i] - 32;
+			else
+				s[i] = s[i];
+		}
+	}
+	s[i] = '\0';
+	return (s);
 }
 
