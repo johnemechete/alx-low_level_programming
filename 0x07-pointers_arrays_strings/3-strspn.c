b957cda8d6e6acffdc879588c2139fac31226744
johnemechete <johnemechete@gmail.com>
2023-01-02 07:16:51 -0800

    3-strspn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -9,13 +9,18 @@
 
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int i, j;
+	unsigned int i, j, count = 0;
 
-	for (i = 0; *(accept + i) != '\0'; i++)
+	for (i = 0; *(s + i) != '\0'; i++)
 	{
-		for (j = 0; *(s + j) != '\0'; j++)
-			if (*(accept + i) == *(s + j))
-				return (j + 1);
+		for (j = 0; *(accept + j) != '\0'; j++)
+			if (*(s + i) == *(accept + j))
+			{
+				count++;
+				break;
+			}
+		if (*(accept + j) == '\0')
+			break;
 	}
-	return (0);
+	return (count);
 }
