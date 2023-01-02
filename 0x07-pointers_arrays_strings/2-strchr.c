97bf43b487da4c64777fcf90db293691717292e4
johnemechete <johnemechete@gmail.com>
2023-01-02 06:27:22 -0800

    2-strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strchr - fills memory with a constant byte
+ * @s: string array
+ * @c: number of bytes
+ * Return: pointer
+ */
+
+char *_strchr(char *s, char c)
+{
+	unsigned int i;
+
+	for (i = 0; *(s + i) != '\0'; i++)
+	{
+		if (*(s + i) == c)
+			return (s + i);
+	}
+	if (c == '\0')
+		return (s + i);
+	return (NULL);
+}
