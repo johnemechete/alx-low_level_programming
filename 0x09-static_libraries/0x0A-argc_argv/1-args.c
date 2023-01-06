47cb7ed24251758b96428cdc287b7179369e2f64
johnemechete <johnemechete@gmail.com>
2023-01-06 09:09:56 -0800

    Task 2

---
+++ 0x09-static_libraries/0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* main - Print the number of agument
+* @argc: Count argument
+* @argv: holds the array of command
+* Return: always return 0
+*/
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc);
+	return (0);
+}
