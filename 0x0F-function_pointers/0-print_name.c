deea3624a2d96c66969eb9b567fa3a9289d098f5
johnemechete <johnemechete@gmail.com>
2023-01-18 06:30:22 -0800

    Task 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name
+ * @name: Holds the name
+ * @f: Point to a function
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
---
+++ 0x0F-function_pointers/_putchar.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _putchar - Print out characters
+ * @c: holds char values
+ */
+
+void _putchar(char c)
+{
+	putchar(c);
+}
