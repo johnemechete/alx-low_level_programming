206e796f9f5d8eb03949a9379ab19eca5bd72139
johnemechete <johnemechete@gmail.com>
2023-01-18 12:13:12 -0800

    Task 1.2

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -12,7 +12,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i;
 
-	if (size != 0 || *(array + 0) != '\0') 
+	if (array == NULL && action == NULL)
 		for (i = 0; i < size; i++)
 			action(array[i]);
 }
