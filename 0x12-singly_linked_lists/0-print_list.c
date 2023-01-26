2ab23ab93f7a87ce028c19839f9a926c8288e0fa
johnemechete <johnemechete@gmail.com>
2023-01-26 14:05:27 -0800

    Task 1

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,13 +10,13 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t i;
+	size_t count;
 
 	if (h == NULL)
 		return (0);
-	i = 1;
+	count = 1;
 	printf("[%d] %s\n", h->len, h->str);
 	if (h->next != NULL)
-		i += print_list(h->next);
-	return (i);
+		count += print_list(h->next);
+	return (count);
 }
